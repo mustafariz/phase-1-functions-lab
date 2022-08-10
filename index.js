60fd8cb5bf689d360101470ee864f70c98aef1d4
syed mustafa ali rizvi <mustafariz15@gmail.com>
2022-08-10 09:30:54 -0700

    commit tree 8c4e1c5fa552a5d0d645db3624da7d424bc91767

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+
+  function distanceFromhqInBlocks(pickupBlock){
+  return Math.abs(42 - pickupBlock)
+  }
+
+
+function distanceFromHqInFeet(pickUpBlock){
+  return distanceFromhqInBlocks (pickUpBlock) * 264
+}
+
+
+function distanceTravelledInFeet(start,end){
+   return Math.abs(start - end)* 264
+}
+
+function calculatesFarePrice(start,destination){
+ const distance = distanceTravelledInFeet(start,destination)
+ if (distance <= 4000){
+   return 0
+ } else if (distnace <= 2000){
+   return distance *0.02
+ }
+}
